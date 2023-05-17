9a936fa772569131826203bfd48f3ec175a55bf4
elaman13 <94231872+elaman13@users.noreply.github.com>
2023-05-17 19:03:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('hi')
+import telebot
+
+bot = telebot.TeleBot('5865282234:AAEVwLn9TQtcaUo1N5fKDwtByBvqWwfiPhc')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+  bot.reply_to(message, f'hi {message.from_user.first_name} ')
